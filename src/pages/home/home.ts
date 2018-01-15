caae452c20cf69547b53a0f7ed533a4db10f75da
bsorrentino <bartolomeo.sorrentino@gmail.com>
2018-01-15 23:22:51 +0100

    update sample

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Broadcaster } from '@ionic-native/broadcaster';
 import { Subscription } from 'rxjs/Subscription';
@@ -11,43 +11,63 @@ import { timer } from 'rxjs/Observable/timer';
 })
 export class HomePage {
 
+  private logs = new Array<string>();
+
   private  subscription:Subscription;
 
-  constructor(public navCtrl: NavController, 
+  constructor(public navCtrl: NavController,
+              private ngZone: NgZone,
               private broadcaster: Broadcaster) {
 
   }
 
 
+  private L( ...args: any[] ) {
+    let v = args.join(' ');
+    console.log(v);
+    this.ngZone.run( () => this.logs.push(v) );
+  }
+
   onRegisterListener() {
-      this.subscription = this.broadcaster.addEventListener("TEST.EVENT")
-        .subscribe( (event:any) => console.log( "event home received", event ));
+    if( this.subscription &&  !this.subscription.closed ) return;
+    const ev = "TEST.EVENT"
+    this.L( "register event", ev);
+    this.subscription = this.broadcaster.addEventListener(ev)
+        .subscribe( event =>
+          this.L( "received", ev, "\n", JSON.stringify(event)));
 
   }
 
   onUnregisterListener() {
+    if( !this.subscription || this.subscription.closed ) return;
+    this.L( "unregister event", "TEST.EVENT");
     this.subscription.unsubscribe();
   }
 
   onStartTest() {
 
-    console.log( cordova.platformId );
+    this.L( "PLATFORM", cordova.platformId );
 
     if( cordova.platformId === "browser" ) {
-
+        const ev = "TEST.EVENT";
         timer(1000,1000)
           .subscribe( (v)=> {
-            console.log( "fire native event");
-            let event = new CustomEvent("TEST.EVENT", { detail: { data:"this is a test event"} } );
+            this.L( "fire native event", ev);
+            let event = new CustomEvent(ev, { detail: { data:"this is a test event"} } );
             document.dispatchEvent( event );
           })
-      
+
     }
     else if( cordova.platformId === "android" ) {
+      const ev = "START.TEST.EVENT";
+      this.L( "fire native event", ev);
+      this.broadcaster.fireNativeEvent( ev, { start:1000, period:1000} );
 
-      this.broadcaster.fireNativeEvent( "START.TEST.EVENT", { start:1000, period:1000} );
     }
 
   }
-  
+
+  onClearLog() {
+    this.ngZone.run( () => this.logs = [] );
+  }
 }
