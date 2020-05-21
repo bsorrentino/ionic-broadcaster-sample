8d34903f44887d780f2c44822c1cdf677b7c991b
bartolomeo sorrentino <bartolomeo.sorrentino@gmail.com>
2020-05-21 23:06:51 +0200

    update

--- node_modules/@ionic/cli-framework/lib/validators.d.ts
+++
@@ -1,7 +0,0 @@
-import { Validator, Validators } from '../definitions';
-export declare function validate(input: string, key: string, validatorsToUse: Validator[]): void;
-export declare const validators: Validators;
-export declare function combine(...validators: Validator[]): Validator;
-export declare function contains(values: (string | undefined)[], { caseSensitive }: {
-    caseSensitive?: boolean;
-}): Validator;
